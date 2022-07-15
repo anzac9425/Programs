07ac40108e3f0c8cd61a9f32dffb7828575fa813
anzac9425 <106593526+anzac9425@users.noreply.github.com>
2022-07-15 23:47:43 +0900

    PROGRAM Beta0.3.2
    /clear, /restart 명령어 추가

--- PROGRAM.bat
+++ PROGRAM.bat
@@ -51,7 +51,7 @@ pause
 :info
 cls
 echo 개발: PH GAMES
-echo 프로그램 버전: beta0.3
+echo 프로그램 버전: beta0.3.2
 echo 프로그램 업데이트: https://github.com/anzac9425/Programs
 pause
 goto menu
@@ -66,10 +66,18 @@ if "%input%" == "/help" (
 	echo /help를 입력하여 도움말을 확인하십시오.
 	echo /exit를 입력하여 메인 메뉴로 이동하십시오.
 	echo /shutdown을 입력하여 이 프로그램을 강제로 종료시키십시오.
+	echo /clear를 입력하여 입력된 모든 문자를 삭제하십시오.
+	echo /restart를 입력하여 프로그램을 재시작 시키십시오.
 	goto command2
 )else if "%input%" == "/exit" (
 	goto menu
 )else if "%input%" == "/shutdown" (
+	exit
+)else if "%input%" == "/clear" (
+	cls
+	goto command
+)else if "%input%" == "/restart" (
+	start PROGRAM.bat
 	exit
 )else (	
 	echo 알 수 없는 명령어입니다.
